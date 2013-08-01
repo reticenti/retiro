6dadbbf9b24955d132ff842623609df1bc633cbb
JH <dontdrinkh2so4@gmail.com>
2013-07-31 17:07:47 -0700

    1

--- src/com/example/retiro/MainActivity.java
+++ src/com/example/retiro/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 	//meeerrrhhhh
     //amac4merrhhhhh
+	//comments
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
